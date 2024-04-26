0afb9bbf1e079496973df87f35beabad485c9f22
anvarinho <anvarinho@gmail.com>
2024-04-26 14:13:22 +0600

    google redirects

--- next.config.js
+++ next.config.js
@@ -46,6 +46,101 @@ const nextConfig = {
         source: '/tours/:slug*', // Use the wildcard :slug* to match all deeper URLs
         destination: '/en/tours/:slug*', // Redirect to the equivalent URL in the /en/places path
         permanent: true,
+      },
+      {
+        source: '/place', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/karakol-ski-base', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/karakol-ski-base', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/karakol', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/karakol-city', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/kichi-alay-valley', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/kichi-alay-valley', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/the-welcome-tour', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/chuy-region-highlights-tour', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/summer-days', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/chuy-ysyk-kol-highlights-tour', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/extended-tour-north-of-kyrgyzstan', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/grand-tour-south-kyrgyzstan', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/sightseeing-tour-of-bishkek', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/sightseeing-tour-of-bishkek', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/extended-tour-kyrgyzstan/', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/grand-tour-kyrgyzstan', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/26', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/kichi-alay-valley', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/tour/6', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/trekking-and-horse-riding-tour', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/chunkurchak-valley-hiking', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/chunkurchak-valley-hiking', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/trekking-and-horse-riding-tour', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/trekking-and-horse-riding-tour', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/magic-of-summer', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/kyrgyzstan-highlights-tour', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/jashyl-kol', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/jashyl-kol', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/tour/extended-tour-north-of-kyrgyzstan/', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/tours/grand-tour-south-kyrgyzstan', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/sary-chelek', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/sary-chelek-biosphere-reserve', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
+      },
+      {
+        source: '/place/Bishkek', // Use the wildcard :slug* to match all deeper URLs
+        destination: '/en/places/bishkek-city', // Redirect to the equivalent URL in the /en/places path
+        permanent: true,
       }
     ];
   },
