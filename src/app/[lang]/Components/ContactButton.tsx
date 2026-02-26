fd0dddac3a0afda88d2c86f256bf11b5ebe355ec
Anvar Jumabaev <anvarinho@Anvars-MacBook-Air.local>
2026-02-26 11:30:32 +0600

    Updated Manas Airport transfer page

--- src/app/[lang]/Components/ContactButton.tsx
+++ src/app/[lang]/Components/ContactButton.tsx
@@ -1,90 +1,102 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import styles from './page.module.css'
-import { usePathname } from 'next/navigation'
+import styles from "./page.module.css";
+import { usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { Locale } from "@/lib/i18n.config";
-import { sendGAEvent } from '@next/third-parties/google'
+import { sendGAEvent } from "@next/third-parties/google";
 import Image from "next/image";
 
 const ContactButton: React.FC<{ lang: Locale }> = ({ lang }) => {
-    const [isScrolled, setIsScrolled] = useState(false);
-    const pathname = usePathname()
-    const contactMe = {
-        en: 'Any Questions?',
-        es: '¿Alguna pregunta?',
-        fr: 'Des questions ?',
-        de: 'Noch Fragen?',
-        it: 'Domande?',
-        ru: 'Связаться?',
-        kr: '질문이 있으신가요?',
-        cn: '有问题吗？',
-        jp: '質問はありますか？',
-        ae: 'هل لديك أي أسئلة؟'
-    }
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathname = usePathname();
+  const contactMe = {
+    en: "Any Questions?",
+    es: "¿Alguna pregunta?",
+    fr: "Des questions ?",
+    de: "Noch Fragen?",
+    it: "Domande?",
+    ru: "Связаться?",
+    kr: "질문이 있으신가요?",
+    cn: "有问题吗？",
+    jp: "質問はありますか？",
+    ae: "هل لديك أي أسئلة؟",
+  };
 
-    const bookTour = {
-        en: 'Book a Tour !',
-        es: 'Reservar Tour !',
-        fr: 'Réserver une visite !',
-        de: 'Tour Buchen !',
-        it: 'Prenota un Tour !',
-        ru: 'Забронировать тур !',
-        kr: '투어 예약 !',
-        cn: '预订旅游 !',
-        jp: 'ツアーを予約する !',
-        ae: '! حجز جولة',
-    }
+  const bookTour = {
+    en: "Book it!",
+    es: "¡Reservar!",
+    fr: "Réserver !",
+    de: "Buchen!",
+    it: "Prenota!",
+    ru: "Забронировать!",
+    kr: "예약!",
+    cn: "预订!",
+    jp: "予約!",
+    ae: "!احجز",
+  };
 
-    useEffect(() => {
-        // Function to handle the scroll event
-        const handleScroll = () => {
-            const scrollY = window.scrollY;
-            // Set a threshold value for when to toggle the class
-            const scrollThreshold = 100;
+  useEffect(() => {
+    // Function to handle the scroll event
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      // Set a threshold value for when to toggle the class
+      const scrollThreshold = 100;
 
-            // Check if the user has scrolled beyond the threshold
-            if (scrollY > scrollThreshold) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
+      // Check if the user has scrolled beyond the threshold
+      if (scrollY > scrollThreshold) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // Add the scroll event listener when the component mounts
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isScrolled]);
+
+  return (
+    <div className={`${styles.contactBtn} ${isScrolled ? "" : styles.active}`}>
+      <div className={styles.contactBox}>
+        {pathname.includes("/tours/") ? (
+          <h4>{bookTour[lang]}</h4>
+        ) : (
+          <h4>{contactMe[lang]}</h4>
+        )}
+        {pathname.includes("/tours/") ? (
+          <Link
+            href={`https://wa.me/${process.env.NEXT_PUBLIC_PHONE_NUMBER}?text=https://central-asia.live${pathname}%20${bookTour[lang]}`}
+            title="Whatsapp contact"
+            target="_blank"
+            onClick={() =>
+              sendGAEvent({
+                event: "buttonClicked",
+                value: "4FJFCJuVic8YELL_7YUq",
+              })
             }
-        };
-
-        // Add the scroll event listener when the component mounts
-        window.addEventListener('scroll', handleScroll);
-
-        // Clean up the scroll event listener when the component unmounts
-        return () => {
-        window.removeEventListener('scroll', handleScroll);
-        };
-    }, [isScrolled]);
-
-    return (
-        <div className={`${styles.contactBtn} ${isScrolled ? '' : styles.active}`}>
-            <div className={styles.contactBox}>
-                {pathname.includes('/tours/') ? (
-                    <h4>{bookTour[lang]}</h4>
-                ) : (
-                    <h4>{contactMe[lang]}</h4>
-                )}
-                {
-                    pathname.includes('/tours/') ? (
-                        <Link href={`https://wa.me/${process.env.NEXT_PUBLIC_PHONE_NUMBER}?text=https://central-asia.live${pathname}%20${bookTour[lang]}`} title="Whatsapp contact" target="_blank" onClick={() => sendGAEvent({ event: 'buttonClicked', value: '4FJFCJuVic8YELL_7YUq' })}>
-                            {/* <FontAwesomeIcon className={styles.phoneIcon} icon={faPhone} /> */}
-                            <Image src={"/whatsapp.png"} width={50} height={50} alt=""/>
-                        </Link>
-                    ) : (
-                        <Link href={`https://wa.me/${process.env.NEXT_PUBLIC_PHONE_NUMBER}?text=Hello!!!`} title="Whatsapp contact" target="_blank">
-                            {/* <FontAwesomeIcon className={styles.phoneIcon} icon={faPhone} /> */}
-                            <Image src={"/whatsapp.png"} width={50} height={50} alt=""/>
-                        </Link>
-                    )
-                }
-            </div>
-        </div>
-    );
+          >
+            {/* <FontAwesomeIcon className={styles.phoneIcon} icon={faPhone} /> */}
+            <Image src={"/whatsapp.png"} width={50} height={50} alt="" />
+          </Link>
+        ) : (
+          <Link
+            href={`https://wa.me/${process.env.NEXT_PUBLIC_PHONE_NUMBER}?text=Hello!!!`}
+            title="Whatsapp contact"
+            target="_blank"
+          >
+            {/* <FontAwesomeIcon className={styles.phoneIcon} icon={faPhone} /> */}
+            <Image src={"/whatsapp.png"} width={50} height={50} alt="" />
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ContactButton;
--- src/app/[lang]/tours/[tourUrl]/meta.tsx
+++ src/app/[lang]/tours/[tourUrl]/meta.tsx
@@ -3,109 +3,111 @@
 import { Locale } from "@/lib/i18n.config";
 
 interface Props {
-  lang: Locale
+  lang: Locale;
   tour: TourInfo; // Assuming 'tour' is a string, adjust the type accordingly if it's different
-  page: any
+  page: any;
 }
 
-const Meta: React.FC<Props> = ({ lang,  tour, page }) => {
-    const data = {
-        "@context": "https://schema.org",
-        "@type": "Product",
-        "name": tour.title,
-        "description": tour.description,
-        "image": tour.images,
-        "sku":tour._id,
-        "brand": {
-            "@type": "Brand",
-            "name": "GuideBook of Kyrgyzstan"
+const Meta: React.FC<Props> = ({ lang, tour, page }) => {
+  const data = {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    name: tour.title,
+    description: tour.description,
+    image: tour.images,
+    sku: tour._id,
+    brand: {
+      "@type": "Brand",
+      name: "GuideBook of Kyrgyzstan",
+    },
+    review: {
+      "@type": "Review",
+      reviewRating: {
+        "@type": "Rating",
+        ratingValue: 4,
+        bestRating: 5,
+      },
+      author: {
+        "@type": "Person",
+        name: "Fred Benson",
+      },
+    },
+    aggregateRating: {
+      "@type": "AggregateRating",
+      ratingValue: 4.9,
+      reviewCount: 189,
+    },
+    offers: {
+      "@type": "Offer",
+      url: `${process.env.NEXT_PUBLIC_URL}/tours/${tour.url}`,
+      priceCurrency: "USD",
+      price: tour.price[0],
+      priceValidUntil: "2024-10-10",
+      availability: "https://schema.org/InStock",
+      hasMerchantReturnPolicy: {
+        "@type": "MerchantReturnPolicy",
+        applicableCountry: "KG",
+        returnPolicyCategory:
+          "https://schema.org/MerchantReturnFiniteReturnWindow",
+        merchantReturnDays: 60,
+        returnMethod: "https://schema.org/ReturnByMail",
+        returnFees: "https://schema.org/FreeReturn",
+      },
+      shippingDetails: {
+        "@type": "OfferShippingDetails",
+        shippingRate: {
+          "@type": "MonetaryAmount",
+          value: 1,
+          currency: "USD",
         },
-        "review": {
-            "@type": "Review",
-            "reviewRating": {
-              "@type": "Rating",
-              "ratingValue": 4,
-              "bestRating": 5
-            },
-            "author": {
-              "@type": "Person",
-              "name": "Fred Benson"
-            }
-          },
-          "aggregateRating": {
-            "@type": "AggregateRating",
-            "ratingValue": 4.9,
-            "reviewCount": 17
+        shippingDestination: {
+          "@type": "DefinedRegion",
+          addressCountry: "KG",
         },
-        "offers": {
-            "@type": "Offer",
-            "url": `${process.env.NEXT_PUBLIC_URL}/tours/${tour.url}`,
-            "priceCurrency": "USD",
-            "price": tour.price[0],
-            "priceValidUntil": "2024-10-10",
-            "availability": "https://schema.org/InStock",
-            "hasMerchantReturnPolicy": {
-              "@type": "MerchantReturnPolicy",
-              "applicableCountry": "KG",
-              "returnPolicyCategory": "https://schema.org/MerchantReturnFiniteReturnWindow",
-              "merchantReturnDays": 60,
-              "returnMethod": "https://schema.org/ReturnByMail",
-              "returnFees": "https://schema.org/FreeReturn",
-            },
-            "shippingDetails": {
-              "@type": "OfferShippingDetails",
-              "shippingRate": {
-                  "@type": "MonetaryAmount",
-                  "value": 1,
-                  "currency": "USD"
-              },
-              "shippingDestination": {
-                "@type": "DefinedRegion",
-                "addressCountry": "KG"
-              },
-              "deliveryTime": {
-                "@type": "ShippingDeliveryTime",
-                "handlingTime": {
-                  "@type": "QuantitativeValue",
-                  "minValue": 1,
-                  "maxValue": 1,
-                  "unitCode": "DAY"
-                },
-                "transitTime": {
-                  "@type": "QuantitativeValue",
-                  "minValue": 1,
-                  "maxValue": 1,
-                  "unitCode": "DAY"
-                }
-              }
-            }
+        deliveryTime: {
+          "@type": "ShippingDeliveryTime",
+          handlingTime: {
+            "@type": "QuantitativeValue",
+            minValue: 1,
+            maxValue: 1,
+            unitCode: "DAY",
           },
-          "breadcrumb": {
-            "@context": "https://schema.org",
-            "@type": "BreadcrumbList",
-            "itemListElement": [
-              {
-                "@type": "ListItem",
-                "position": 1,
-                "name": page.tours.name,
-                "item": `${process.env.NEXT_PUBLIC_URL}/${lang}/tours`
-              },
-              {
-                "@type": "ListItem",
-                "position": 2,
-                "name": `${tour.title}`,
-                "item": `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`
-              }
-            ]
+          transitTime: {
+            "@type": "QuantitativeValue",
+            minValue: 1,
+            maxValue: 1,
+            unitCode: "DAY",
           },
-    }
-    
-    return (
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-      />
-    );
+        },
+      },
+    },
+    breadcrumb: {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: page.tours.name,
+          item: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours`,
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: `${tour.title}`,
+          item: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
+        },
+      ],
+    },
   };
-  
-  export default Meta;
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+};
+
+export default Meta;
+
