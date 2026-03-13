af195927f83fbefd8509745367f4eef81c46f189
Anvar Jumabaev <anvarinho@Anvars-MacBook-Air.local>
2026-03-13 16:04:05 +0600

    links and images in tour description

--- src/app/[lang]/places/[placeUrl]/meta.tsx
+++ src/app/[lang]/places/[placeUrl]/meta.tsx
@@ -1,95 +1,94 @@
 import { Locale } from "@/lib/i18n.config";
 
 interface Props {
-    place: Place; // Assuming 'tour' is a string, adjust the type accordingly if it's different
-    lang: Locale
-    page: any
+  place: Place; // Assuming 'tour' is a string, adjust the type accordingly if it's different
+  lang: Locale;
+  page: any;
 }
 
 const Meta: React.FC<Props> = ({ lang, place, page }) => {
-  
-    const data = {
+  const data = {
+    "@context": "https://schema.org",
+    "@type": "Place",
+    name: place.name,
+    title: place.title,
+    description: place.description.substring(0, 220),
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: place.region,
+      addressCountry: "Kyrgyzstan",
+    },
+    author: {
+      "@type": "Person",
+      name: "Anvar Jumabaev",
+    },
+    publisher: {
+      "@type": "Organization",
+      name: "GuideBook of Kyrgyzstan",
+      logo: {
+        "@type": "ImageObject",
+        url: `${process.env.NEXT_PUBLIC_URL}/favicon.ico`,
+      },
+    },
+    image: `${process.env.NEXT_PUBLIC_URL}/${place.images[0]}`,
+    telephone: `${process.env.NEXT_PUBLIC_PHONE_NUMBER}`,
+    geo: {
+      "@type": "GeoCoordinates",
+      latitude: place.location.latitude,
+      longitude: place.location.longitude,
+    },
+    hasMap: `${process.env.NEXT_PUBLIC_URL}/${lang}/places/${place.url}`,
+    breadcrumb: {
       "@context": "https://schema.org",
-      "@type": "Place",
-      "name": place.name,
-      "title": place.title,
-      "description": place.description.substring(0, 220),
-      "address": {
-        "@type": "PostalAddress",
-        "addressLocality": place.region,
-        "addressCountry": "Kyrgyzstan"
-      },
-      author: {
-        "@type": "Person",
-        name: "Anvar Jumabaev"
-      },
-      publisher: {
-        "@type": "Organization",
-        name: "GuideBook of Kyrgyzstan",
-        logo: {
-          "@type": "ImageObject",
-          url: `${process.env.NEXT_PUBLIC_URL}/favicon.ico`
-        }
-      },
-		"image":`${process.env.NEXT_PUBLIC_URL}/${place.images[0]}`,
-      "telephone": `${process.env.NEXT_PUBLIC_PHONE_NUMBER}`,
-      "geo": {
-        "@type": "GeoCoordinates",
-        "latitude": place.location.latitude,
-        "longitude": place.location.longitude
-      },
-      "hasMap": `${process.env.NEXT_PUBLIC_URL}/${lang}/places/${place.url}`,
-      "breadcrumb": {
-        "@context": "https://schema.org",
-        "@type": "BreadcrumbList",
-        "itemListElement": [
-          {
-            "@type": "ListItem",
-            "position": 1,
-            "name": page.sights.name,
-            "item": `${process.env.NEXT_PUBLIC_URL}/${lang}/places`
-          },
-          {
-            "@type": "ListItem",
-            "position": 2,
-            "name": `${place.name}`,
-            "item": `${process.env.NEXT_PUBLIC_URL}/${lang}/places/${place.url}`
-          }
-        ]
-      },
-      "openingHoursSpecification": {
-        "@type": "OpeningHoursSpecification",
-        "dayOfWeek": [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday"
-        ],
-        "opens": "09:00",
-        "closes": "18:00"
-      },
-      sameAs: [
-          `${process.env.NEXT_PUBLIC_URL}/en/places/${place.url}`,
-          `${process.env.NEXT_PUBLIC_URL}/ru/places/${place.url}`
+      "@type": "BreadcrumbList",
+      itemListElement: [
+        {
+          "@type": "ListItem",
+          position: 1,
+          name: page.sights.name,
+          item: `${process.env.NEXT_PUBLIC_URL}/${lang}/places`,
+        },
+        {
+          "@type": "ListItem",
+          position: 2,
+          name: `${place.name}`,
+          item: `${process.env.NEXT_PUBLIC_URL}/${lang}/places/${place.url}`,
+        },
       ],
-      additionalProperty: [
-          {
-            "@type": "PropertyValue",
-            "name": "Category",
-            "value": "Tourist Attraction"
-          }
-      ]
-    }
-    
-    return (
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-      />
-    );
+    },
+    openingHoursSpecification: {
+      "@type": "OpeningHoursSpecification",
+      dayOfWeek: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      opens: "09:00",
+      closes: "18:00",
+    },
+    sameAs: [
+      `${process.env.NEXT_PUBLIC_URL}/en/places/${place.url}`,
+      `${process.env.NEXT_PUBLIC_URL}/ru/places/${place.url}`,
+    ],
+    additionalProperty: [
+      {
+        "@type": "PropertyValue",
+        name: "Category",
+        value: "Tourist Attraction",
+      },
+    ],
   };
-  
-  export default Meta;
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+};
+
+export default Meta;
--- src/app/[lang]/tours/[tourUrl]/page.tsx
+++ src/app/[lang]/tours/[tourUrl]/page.tsx
@@ -1,181 +1,210 @@
-import { Locale } from '@/lib/i18n.config'
-import getTour from '@/lib/getTour';
-import styles from './page.module.css'
-import { Metadata } from 'next'
-import { Suspense } from "react"
-import LoadingSpinner from '../../Components/LoadingSpinner';
-import DayView from './DayView';
-import { getDictionary } from '@/lib/dictionary'
-import Link from 'next/link';
-import Meta from './meta';
-import { notFound } from 'next/navigation'
-import ImageRenderer from '../../Components/image/ImageRenderer';
+import { Locale } from "@/lib/i18n.config";
+import getTour from "@/lib/getTour";
+import styles from "./page.module.css";
+import { Metadata } from "next";
+import { Suspense } from "react";
+import LoadingSpinner from "../../Components/LoadingSpinner";
+import DayView from "./DayView";
+import { getDictionary } from "@/lib/dictionary";
+import Link from "next/link";
+import Meta from "./meta";
+import { notFound } from "next/navigation";
+import ImageRenderer from "../../Components/image/ImageRenderer";
+import TourDescription from "./TourDescription";
 
 type Params = {
-    params: {
-        tourUrl: string,
-        lang: Locale
-    }
+  params: {
+    tourUrl: string;
+    lang: Locale;
+  };
+};
+
+export default async function Tour({ params: { tourUrl, lang } }: Params) {
+  // const baseUrl = `${process.env.NEXT_PUBLIC_URL}/`;
+  const { page } = await getDictionary(lang);
+  const tourData: Promise<TourInfo> = getTour(tourUrl, lang);
+  const data = await tourData;
+  if (!data) notFound();
+  return (
+    <div className={styles.main}>
+      <Meta lang={lang} tour={data} page={page} />
+      <Suspense fallback={<LoadingSpinner text={"Loading"} />}>
+        {/* <JsonLD data={metaData} /> */}
+        <article className={styles.article}>
+          <h1>{data.title}</h1>
+          <ImageRenderer images={data.images} priority />
+          <div className={styles.meta}>
+            <p>
+              <strong>{page.tours.tourPage.level}</strong>: {data.level}
+            </p>
+            <p>
+              <strong>{page.tours.tourPage.duration}</strong>:{" "}
+              {data.days.length}{" "}
+              {data.days.length == 1
+                ? `${page.tours.tourPage.day}`
+                : `${page.tours.tourPage.days}`}
+            </p>
+            <p>
+              <strong>{page.tours.tourPage.price}</strong>:{" "}
+              {data.price[data.price.length - 1]}$
+            </p>
+          </div>
+          <TourDescription description={data.description} lang={lang} />
+          <h3>{page.tours.tourPage.details}</h3>
+          <div className={styles.daysNavigation}>
+            {data.days.map((day, index) => (
+              <Link href={`#day${index + 1}`} key={index}>
+                {page.tours.tourPage.day}: {index + 1}
+              </Link>
+            ))}
+            <Link href="#note">{page.tours.tourPage.note}</Link>
+          </div>
+          <br />
+          {data.days.map((day, index) => (
+            <DayView
+              key={index}
+              params={{ day: day, index: index, lang: lang }}
+            />
+          ))}
+          <br />
+          <div className={styles.extra} id="note">
+            <h3 className={styles.infoTitle}>{page.tours.tourPage.addInfo}</h3>
+            <div className={styles.infoList}>
+              {data.price.length > 1 && (
+                <div>
+                  <h4>{page.tours.tourPage.price}</h4>
+                  {data.price.map((fee, index) => (
+                    <p key={index}>
+                      {index + 1}{" "}
+                      {index === data.price.length - 1
+                        ? page.tours.tourPage.andMore
+                        : index === 0
+                          ? page.tours.tourPage.person
+                          : page.tours.tourPage.persons}
+                      : {fee}$
+                    </p>
+                  ))}
+                </div>
+              )}
+              <div>
+                <h4>{page.tours.tourPage.includings}</h4>
+                {data.includings.map((text, index) => (
+                  <p key={index}>{text}</p>
+                ))}
+              </div>
+              <div>
+                <h4>{page.tours.tourPage.excludings}</h4>
+                {data.excludings.map((text, index) => (
+                  <p key={index}>{text}</p>
+                ))}
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className={styles.essentials}>
+            <h3>{page.tours.TourPreparation.title}</h3>
+            <p>{page.tours.TourPreparation.Description}</p>
+            {Object.entries(page.tours.TourPreparation.Options).map(
+              ([key, value]: [string, string]) => (
+                <p key={key}>
+                  <b>{key}</b> : {value}
+                </p>
+              ),
+            )}
+            <h4>{page.tours.TourPreparation.overview}</h4>
+          </div>
+        </article>
+      </Suspense>
+    </div>
+  );
 }
 
-export default async function Tour({ params: {tourUrl, lang}}: Params) {
-    // const baseUrl = `${process.env.NEXT_PUBLIC_URL}/`;
-    const { page } = await getDictionary(lang)
-    const tourData: Promise<TourInfo> = getTour(tourUrl, lang)
-    const data = await tourData
-    if (!data) notFound()
-    return (
-        <div className={styles.main}>
-            <Meta lang={lang} tour={data} page={page}/>
-            <Suspense fallback={<LoadingSpinner text={"Loading"}/>}>
-                {/* <JsonLD data={metaData} /> */}
-                <article className={styles.article}>
-                    <h1>{data.title}</h1>
-                    <ImageRenderer images={data.images} priority/>
-                    <div className={styles.meta}>
-                        <p><strong>{page.tours.tourPage.level}</strong>: {data.level}</p>
-                        <p><strong>{page.tours.tourPage.duration}</strong>: {data.days.length} {data.days.length == 1 ? `${page.tours.tourPage.day}`: `${page.tours.tourPage.days}`}</p>
-                        <p><strong>{page.tours.tourPage.price}</strong>: {data.price[data.price.length - 1]}$</p>
-                    </div>
-                    <h2>{data.description}</h2>
-                    <h3>{page.tours.tourPage.details}</h3>
-                    <div className={styles.daysNavigation}>
-                        {data.days.map((day, index) => (
-                            <Link href={`#day${index + 1}`} key={index}>{page.tours.tourPage.day}: {index + 1}</Link>
-                        ))}
-                        <Link href="#note">{page.tours.tourPage.note}</Link>
-                    </div>
-                    <br />
-                    {data.days.map((day, index) => (
-                        <DayView key={index} params={{ day: day, index: index, lang: lang }} />
-                    ))}
-                    <br />
-                    <div className={styles.extra} id='note'>
-                        <h3 className={styles.infoTitle}>{page.tours.tourPage.addInfo}</h3>
-                        <div className={styles.infoList}>
-                            {data.price.length > 1 && (
-                                <div>
-                                    <h4>{page.tours.tourPage.price}</h4>
-                                    {data.price.map((fee, index) => (
-                                        <p key={index}>
-                                            {index + 1} {index === data.price.length - 1 ? page.tours.tourPage.andMore : (index === 0 ? page.tours.tourPage.person : page.tours.tourPage.persons)}: {fee}$
-                                        </p>
-                                    ))}
-                                </div>
-                            )}
-                            <div>
-                                <h4>{page.tours.tourPage.includings}</h4>
-                                {data.includings.map((text, index) => (
-                                    <p key={index}>{text}</p>
-                                ))}
-                            </div>
-                            <div>
-                                <h4>{page.tours.tourPage.excludings}</h4>
-                                {data.excludings.map((text, index) => (
-                                    <p key={index}>{text}</p>
-                                ))}
-                            </div>
-                        </div>
-                    </div>  
-                    <br />
-                    <div className={styles.essentials}>
-                        <h3>{page.tours.TourPreparation.title}</h3>
-                        <p>{page.tours.TourPreparation.Description}</p>
-                        {Object.entries(page.tours.TourPreparation.Options).map(([key, value]: [string, string]) => (
-                            <p key={key}><b>{key}</b> : {value}</p>
-                        ))}
-                        <h4>{page.tours.TourPreparation.overview}</h4>
-                    </div>
-                </article>  
-            </Suspense>
-        </div>
-    )
-}
-
-export async function generateMetadata({ params: {tourUrl, lang}}: Params): Promise<Metadata> {
-    const tourData: Promise<TourInfo> = getTour(tourUrl, lang)
-    const tour = await tourData
-    if (!tour) notFound()
-    const baseUrl = `${process.env.NEXT_PUBLIC_URL}/`;
-    const description = tour.description.substring(0, 159)
-    const { page } = await getDictionary(lang)
-    return {
-        title: {
-          absolute: tour.title
-        },
-        description: description,
-        keywords: tour.keywords,
-        applicationName: 'GuideBook of Kyrgyzstan',
-        category: "Travel",
-        openGraph: {
-            title: tour.title + ' | ' + 'GuideBook of Kyrgyzstan',
-            description: description,
-            url: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
-            siteName: 'GuideBook of Kyrgyzstan',
-            images: {
-                url: `${process.env.NEXT_PUBLIC_URL}/${tour.images[0]}`,
-                secureUrl: `${process.env.NEXT_PUBLIC_URL}/${tour.images[0]}`,
-                width: 800,
-                height: 600,
-                alt: tour.title,
-                type:"image/jpeg"
-            },
-            locale: page.langCode.replace("-",'_'),
-            type: 'website',
-        },
-        alternates: {
-            canonical: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
-            languages: {
-                "en-US": `${process.env.NEXT_PUBLIC_URL}/en/tours/${tour.url}`,
-                "fr-FR": `${process.env.NEXT_PUBLIC_URL}/fr/tours/${tour.url}`,
-                "de-DE": `${process.env.NEXT_PUBLIC_URL}/de/tours/${tour.url}`,
-                "es-ES": `${process.env.NEXT_PUBLIC_URL}/es/tours/${tour.url}`,
-                "ru-RU": `${process.env.NEXT_PUBLIC_URL}/ru/tours/${tour.url}`,
-                "it-IT": `${process.env.NEXT_PUBLIC_URL}/it/tours/${tour.url}`,
-                "ja-JP": `${process.env.NEXT_PUBLIC_URL}/jp/tours/${tour.url}`,
-                "ko-KR": `${process.env.NEXT_PUBLIC_URL}/kr/tours/${tour.url}`,
-                "ar-AE": `${process.env.NEXT_PUBLIC_URL}/ae/tours/${tour.url}`,
-                "zh-CN": `${process.env.NEXT_PUBLIC_URL}/cn/tours/${tour.url}`
-            }
-        },
-        twitter: {
-            card: "summary_large_image",
-            title: tour.title,
-            description: description,
-            siteId: "",
-            creator: "@anvarinho",
-            creatorId: "@anvarinho",
-            images: {
-                url: `${process.env.NEXT_PUBLIC_URL}/${tour.images[0]}`,
-                width: 800,
-                height: 600,
-                alt: tour.title
-            }
-        },
-        appLinks: {
-            ios: {
-              url: "https://apps.apple.com/us/app/guidebook-kyrgyzstan/id1575382810",
-              app_store_id: "id1575382810",
-              app_name: "GuideBook of Kyrgyzstan"
-            },
-            android: {
-              url: "https://play.google.com/store/apps/details?id=com.anvarinho.guidebook",
-              package: "com.anvarinho.guidebook",
-              app_name: "GuideBook of Kyrgyzstan"
-            },
-            web: {
-              url: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
-              should_fallback: true,
-            }
-        },
-        robots: {
-            index: true,
-            follow: true,
-            nocache: true,
-            googleBot: {
-                index: true,
-                follow: true,
-            }
-        }
-    }
+export async function generateMetadata({
+  params: { tourUrl, lang },
+}: Params): Promise<Metadata> {
+  const tourData: Promise<TourInfo> = getTour(tourUrl, lang);
+  const tour = await tourData;
+  if (!tour) notFound();
+  const baseUrl = `${process.env.NEXT_PUBLIC_URL}/`;
+  const description = tour.description.substring(0, 159);
+  const { page } = await getDictionary(lang);
+  return {
+    title: {
+      absolute: tour.title,
+    },
+    description: description,
+    keywords: tour.keywords,
+    applicationName: "GuideBook of Kyrgyzstan",
+    category: "Travel",
+    openGraph: {
+      title: tour.title + " | " + "GuideBook of Kyrgyzstan",
+      description: description,
+      url: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
+      siteName: "GuideBook of Kyrgyzstan",
+      images: {
+        url: `${process.env.NEXT_PUBLIC_URL}/${tour.images[0]}`,
+        secureUrl: `${process.env.NEXT_PUBLIC_URL}/${tour.images[0]}`,
+        width: 800,
+        height: 600,
+        alt: tour.title,
+        type: "image/jpeg",
+      },
+      locale: page.langCode.replace("-", "_"),
+      type: "website",
+    },
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
+      languages: {
+        "en-US": `${process.env.NEXT_PUBLIC_URL}/en/tours/${tour.url}`,
+        "fr-FR": `${process.env.NEXT_PUBLIC_URL}/fr/tours/${tour.url}`,
+        "de-DE": `${process.env.NEXT_PUBLIC_URL}/de/tours/${tour.url}`,
+        "es-ES": `${process.env.NEXT_PUBLIC_URL}/es/tours/${tour.url}`,
+        "ru-RU": `${process.env.NEXT_PUBLIC_URL}/ru/tours/${tour.url}`,
+        "it-IT": `${process.env.NEXT_PUBLIC_URL}/it/tours/${tour.url}`,
+        "ja-JP": `${process.env.NEXT_PUBLIC_URL}/jp/tours/${tour.url}`,
+        "ko-KR": `${process.env.NEXT_PUBLIC_URL}/kr/tours/${tour.url}`,
+        "ar-AE": `${process.env.NEXT_PUBLIC_URL}/ae/tours/${tour.url}`,
+        "zh-CN": `${process.env.NEXT_PUBLIC_URL}/cn/tours/${tour.url}`,
+      },
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: tour.title,
+      description: description,
+      siteId: "",
+      creator: "@anvarinho",
+      creatorId: "@anvarinho",
+      images: {
+        url: `${process.env.NEXT_PUBLIC_URL}/${tour.images[0]}`,
+        width: 800,
+        height: 600,
+        alt: tour.title,
+      },
+    },
+    appLinks: {
+      ios: {
+        url: "https://apps.apple.com/us/app/guidebook-kyrgyzstan/id1575382810",
+        app_store_id: "id1575382810",
+        app_name: "GuideBook of Kyrgyzstan",
+      },
+      android: {
+        url: "https://play.google.com/store/apps/details?id=com.anvarinho.guidebook",
+        package: "com.anvarinho.guidebook",
+        app_name: "GuideBook of Kyrgyzstan",
+      },
+      web: {
+        url: `${process.env.NEXT_PUBLIC_URL}/${lang}/tours/${tour.url}`,
+        should_fallback: true,
+      },
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: true,
+      },
+    },
+  };
 }
